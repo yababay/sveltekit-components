ae7eb40aa33dd810f4d7d8ffe896ab0af34c6445
mabel <bigbelk@live.ru>
2023-11-13 02:06:40 +0300

    article

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,9 +1,11 @@
 //import Tags from '$lib/components/tags/index.svelte'
 import Header from '$lib/components/header/index.svelte'
 import Footer from '$lib/components/footer/index.svelte'
+import Article from '$lib/components/article/index.svelte'
 
 export {
     Header,
     Footer,
+    Article,
     //Tags
 }
